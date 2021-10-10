57b77f7e660e7ad0badc95d4ddf616bcba15e1ba
Ederish <63983547+Ederish@users.noreply.github.com>
2021-10-10 11:40:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,13 @@
-;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'meditaciones_v1',
   urlsToCache = [
     './',
-    'https://fonts.googleapis.com/css?family=Raleway:400,700',
-    'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
     'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
-    'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './img/ProgramadorFitness.png',
+    './img/Logo.png',
     './img/favicon.png'
-  ]
-
+]
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -24,7 +19,6 @@ self.addEventListener('install', e => {
       .catch(err => console.log('Falló registro de cache', err))
   )
 })
-
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
   const cacheWhitelist = [CACHE_NAME]
